bab65d40ef5d6d8d6b7bdb9fd2e3740b01dc14cd
la785m <lalvesdasilva@wayfair.com>
2020-11-10 19:29:27 +0100

    refactoring

--- src/app.js
+++ src/app.js
@@ -3,4 +3,4 @@ console.log("Hellow world!!!!");
 var template = <p>This is JSX from app.js!22</p>;
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot )
+ReactDOM.render(template, appRoot)
