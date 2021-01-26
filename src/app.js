db006b5bb331f8c3bea1f2b19ce83c532b2780c9
la785m <lalvesdasilva@wayfair.com>
2021-01-26 02:08:36 +0100

    adding react and dom locally + utils file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("running app!!");
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const template = React.createElement('h1', {onLoad: startCursor}, "dummy text !!!!!!!");
+console.log("hi");
+ReactDOM.render(template, document.getElementById('app'))
+
